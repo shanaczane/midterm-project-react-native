cc02de49f1385c9dcaaa3bdf19a2b87d54a50a2e
Shana Czane M. Cruzat <shanaczanecruzat@gmail.com>
2026-03-02 12:10:59 +0800

    feat: add Job interface and update navigation param types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,26 @@
-// Navigation param lists
+export interface Job {
+  id: string;
+  title: string;
+  mainCategory: string;
+  companyName: string;
+  companyLogo?: string;
+  jobType: string;
+  workModel: string;
+  seniorityLevel: string;
+  minSalary?: number;
+  maxSalary?: number;
+  currency?: string;
+  locations: string[];
+  tags: string[];
+  description?: string;
+  pubDate?: number;
+  expiryDate?: number;
+  applicationLink?: string;
+}
+
 export type RootStackParamList = {
   Main: undefined;
-  ApplicationForm: undefined;
+  ApplicationForm: { job: Job; fromSavedJobs?: boolean };
 };
 
 export type TabParamList = {
