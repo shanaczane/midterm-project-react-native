9f5fe6d786aa025155d7c19307749277fa5cdc0a
Shan8 <shanaczanecruzat@gmail.com>
2026-03-06 16:15:22 +0800

    fix: remove white rectangle by passing app theme to NavigationContainer

--- App.tsx
+++ App.tsx
@@ -1,18 +1,37 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme, DarkTheme } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { ThemeProvider } from "./src/context/ThemeContext";
+import { ThemeProvider, useTheme } from "./src/context/ThemeContext";
 import { SavedJobProvider } from "./src/context/SavedJobContext";
 import AppNavigator from "./src/navigation/AppNavigator";
+
+const AppContent = () => {
+  const { theme, isDark } = useTheme();
+
+  const navTheme = {
+    ...(isDark ? DarkTheme : DefaultTheme),
+    colors: {
+      ...(isDark ? DarkTheme.colors : DefaultTheme.colors),
+      background: theme.background,
+      card: theme.background,
+      text: theme.text,
+      border: theme.border,
+    },
+  };
+
+  return (
+    <NavigationContainer theme={navTheme}>
+      <AppNavigator />
+    </NavigationContainer>
+  );
+};
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <ThemeProvider>
         <SavedJobProvider>
-          <NavigationContainer>
-            <AppNavigator />
-          </NavigationContainer>
+          <AppContent />
         </SavedJobProvider>
       </ThemeProvider>
     </SafeAreaProvider>
